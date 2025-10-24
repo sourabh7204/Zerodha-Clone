114a4fae1e9a05005a0824fec0bcd156945a4b08
sourabh7204 <sourabh722004@gmail.com>
2025-10-24 22:47:53 +0530

    Updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 import HomePage from "./landing_page/home/HomePage";
 import SignUp from "./landing_page/signup/SignUp";
 import AboutPage from "./landing_page/about/AboutPage";
-import ProductPage from "./landing_page/product/ProductPage";
+// import ProductPage from "./landing_page/product/ProductPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
 
@@ -22,10 +22,10 @@ root.render(
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/about" element={<AboutPage />} />
-      <Route path="/product" element={<ProductPage />} />
+      {/* <Route path="/product" element={<ProductPage />} /> */}
       <Route path="/pricing" element={<PricingPage />} />
       <Route path="/support" element={<SupportPage />} />
-      <Route path="*" element={<NotFound />} />
+      {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
     <Footer />
   </BrowserRouter>
