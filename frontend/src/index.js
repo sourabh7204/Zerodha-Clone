b4073311d64485ad1bfc5394dc3e15008f5d3470
sourabh7204 <sourabh722004@gmail.com>
2025-10-24 22:48:42 +0530

    Updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,7 +25,7 @@ root.render(
       {/* <Route path="/product" element={<ProductPage />} /> */}
       <Route path="/pricing" element={<PricingPage />} />
       <Route path="/support" element={<SupportPage />} />
-      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
     <Footer />
   </BrowserRouter>
