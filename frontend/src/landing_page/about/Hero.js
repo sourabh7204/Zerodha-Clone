3b553aa41a846c76f187499b2a7b9213d754eebd
sourabh7204 <sourabh722004@gmail.com>
2025-11-05 22:30:07 +0530

    Building Hero Page for About Section

--- frontend/src/landing_page/about/Hero.js
+++ frontend/src/landing_page/about/Hero.js
@@ -1,13 +1,71 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
     <div className="container">
-      <div className="row">
-        <h1>
-          We pioneered the discount broking model in India. Now, we are breaking
-          ground with our technology.
-        </h1>
+      <div className="row p-5 mb-5 mt-5">
+        <h2 className="fs-3 text-center">
+          We pioneered the discount broking model in India. <br /> Now, we are
+          breaking ground with our technology.
+        </h2>
+      </div>
+
+      <div
+        className="row p-5 mt-5 border-top text-muted"
+        style={{ lineHeight: "1.8", fontSize: "1rem" }}
+      >
+        <div className="col-6 p-5">
+          <p>
+            <p>
+              We kick-started operations on the 15th of August, 2010 with the
+              goal of breaking all barriers that traders and investors face in
+              India in terms of cost, support, and technology. We named the
+              company Zerodha, a combination of Zero and "Rodha", the Sanskrit
+              word for barrier.
+            </p>
+            <p>
+              Today, our disruptive pricing models and in-house technology have
+              made us the biggest stock broker in India.
+            </p>
+            <p>
+              Over 1.6+ crore clients place billions of orders every year
+              through our powerful ecosystem of investment platforms,
+              contributing over 15% of all Indian retail trading volumes.
+            </p>
+          </p>
+        </div>
+        <div className="col-6 p-5">
+          <p>
+            <p>
+              In addition, we run a number of popular open online educational
+              and community initiatives to empower retail traders and investors.
+            </p>
+            <p>
+              <Link href="" style={{ textDecoration: "none" }}>
+                Rainmatter
+              </Link>
+              , our fintech fund and incubator, has invested in several fintech
+              startups with the goal of growing the Indian capital markets.
+            </p>
+            <p>
+              And yet, we are always up to something new every day. Catch up on
+              the latest updates on our{" "}
+              <Link href="" style={{ textDecoration: "none" }}>
+                blog
+              </Link>{" "}
+              or see what the media is{" "}
+              <Link href="" style={{ textDecoration: "none" }}>
+                saying about us
+              </Link>{" "}
+              or learn more about our business and product{" "}
+              <Link href="" style={{ textDecoration: "none" }}>
+                philosophies
+              </Link>
+              .
+            </p>
+          </p>
+        </div>
       </div>
     </div>
   );
