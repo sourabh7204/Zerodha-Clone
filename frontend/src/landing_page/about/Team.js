34a8b609a44e0e0df3c4ef58c16d0b4c21fb8c5d
sourabh7204 <sourabh722004@gmail.com>
2025-11-09 22:09:45 +0530

    Building Hero Page for About Section

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -3,23 +3,23 @@ import React from "react";
 function Team() {
   return (
     <div className="container">
-      <div className="row p-5 border-top">
+      <div className="row p-3 border-top">
         <h2 className="fs-3   text-center">People</h2>
       </div>
 
       <div
-        className="row p-5   text-muted"
+        className="row p-3   text-muted"
         style={{ lineHeight: "1.8", fontSize: "1rem" }}
       >
-        <div className="col-6 p-5 text-center">
+        <div className="col-6 p-3 text-center">
           <img
             src="media/images/nithinKamath.jpg"
-            style={{ borderRadius: "100%", width: "65%" }}
+            style={{ borderRadius: "100%", width: "50%" }}
           />
-          <h4>Nithin Kamath</h4>
-          <h5>Founder, CEO</h5>
+          <h5 className="mt-3">Nithin Kamath</h5>
+          <h6>Founder, CEO</h6>
         </div>
-        <div className="col-6 p-5">
+        <div className="col-6 p-3">
           <p>
             Nithin bootstrapped and founded Zerodha in 2010 to overcome the
             hurdles he faced during his decade long stint as a trader. Today,
@@ -29,9 +29,20 @@ function Team() {
             He is a member of the SEBI Secondary Market Advisory Committee
             (SMAC) and the Market Data Advisory Committee (MDAC).
           </p>
+          <p>Playing basketball is his zen.</p>
           <p>
-            Playing basketball is his zen. Connect on Homepage / TradingQnA /
-            Twitter
+            Connect on{" "}
+            <a href="" style={{ textDecoration: "none" }}>
+              Homepage
+            </a>{" "}
+            /
+            <a href="" style={{ textDecoration: "none" }}>
+              TradingQnA
+            </a>{" "}
+            /
+            <a href="" style={{ textDecoration: "none" }}>
+              Twitter
+            </a>
           </p>
         </div>
       </div>
